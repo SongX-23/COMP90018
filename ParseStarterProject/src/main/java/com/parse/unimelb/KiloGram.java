6661676f7cdf19f657f487254807e15e6f759c4c
Song Xue <sxue1@student.unimelb.edu.au>
2015-09-22 14:36:13 +1000

    Add signup function

--- ParseStarterProject/src/main/java/com/parse/unimelb/KiloGram.java
+++ ParseStarterProject/src/main/java/com/parse/unimelb/KiloGram.java
@@ -36,24 +36,6 @@ public class KiloGram extends Application {
     // defaultACL.setPublicReadAccess(true);
     ParseACL.setDefaultACL(defaultACL, true);
 
-    // Dummy user
-    ParseUser user = new ParseUser();
-    user.setUsername("my name");
-    user.setPassword("my pass");
-    user.setEmail("email@example.com");
 
-// other fields can be set just like with ParseObject
-    user.put("phone", "650-555-0000");
-
-    user.signUpInBackground(new SignUpCallback() {
-      public void done(ParseException e) {
-        if (e == null) {
-          // Hooray! Let them use the app now.
-        } else {
-          // Sign up didn't succeed. Look at the ParseException
-          // to figure out what went wrong
-        }
-      }
-    });
   }
 }
--- ParseStarterProject/src/main/java/com/parse/unimelb/SignupActivity.java
+++ ParseStarterProject/src/main/java/com/parse/unimelb/SignupActivity.java
@@ -3,6 +3,7 @@ package com.parse.unimelb;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 import com.parse.unimelb.R;
 
 public class SignupActivity extends AppCompatActivity {
@@ -18,7 +22,7 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        //get the login text
+        // get the login text
         TextView loginTextView = (TextView) this.findViewById(R.id.loginTextView);
         loginTextView.setOnClickListener(new View.OnClickListener() {
 
@@ -28,17 +32,40 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //signup button
+        //signup 
         Button signupButton = (Button) this.findViewById(R.id.signupButton);
         EditText first_name = (EditText) this.findViewById(R.id.firstNameEditText);
         EditText last_name = (EditText) this.findViewById(R.id.lastNameEditText);
         EditText email = (EditText) this.findViewById(R.id.emailEditText);
         EditText password = (EditText) this.findViewById(R.id.passwordEditText);
+        String firstname = first_name.getText().toString();
+        String lastname = last_name.getText().toString();
+        final String email_str = email.getText().toString();
+        final String psw = password.getText().toString();
+        final String username = firstname + " " + lastname;
         signupButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
         public void onClick(View v){
+                // Dummy user
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(psw);
+                user.setEmail(email_str);
 
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Hooray! Let them use the app now.
+                            Log.v("Sign up successfully.", e.toString());
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                        }
+                    }
+                });
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
