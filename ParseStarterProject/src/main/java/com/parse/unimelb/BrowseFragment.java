8731dcda766d7b48bfde855087f8a6e9a01aa5f9
Song Xue <sxue1@student.unimelb.edu.au>
2015-10-02 14:26:31 +1000

    Fetch the feeds from API Getting photo, user profile photo, user name, likes, comments, locations

--- ParseStarterProject/src/main/java/com/parse/unimelb/BrowseFragment.java
+++ ParseStarterProject/src/main/java/com/parse/unimelb/BrowseFragment.java
@@ -1,16 +1,24 @@
 package com.parse.unimelb;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.android.volley.Cache;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.parse.unimelb.R;
@@ -18,6 +26,8 @@ import com.parse.unimelb.R;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,6 +48,7 @@ public class BrowseFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private RequestQueue mRequestQueue;
 
     /**
      * Use this factory method to create a new instance of
@@ -75,6 +86,12 @@ public class BrowseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Cache cache = new DiskBasedCache(getActivity().getCacheDir(), 1024*1024);
+        BasicNetwork network = new BasicNetwork(new HurlStack());
+
+        mRequestQueue = new RequestQueue(cache, network);
+        mRequestQueue.start();
+        loadFeeds();
         return inflater.inflate(R.layout.fragment_browse, container, false);
     }
 
@@ -130,6 +147,116 @@ public class BrowseFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+    public void loadFeeds(){
+        //request url
+        //String request_url = FeedFragment.getResources().getString(R.string.instagram_api_url)
+        //        + FeedFragment.getResources().getString(R.string.instagram_api_users_method)
+        //        + "self/feed?access_token="
+        //        + FeedFragment.getResources().getString(R.string.instagram_access_token);
+        String request_url = "https://api.instagram.com/v1/users/self/feed?access_token=25846960.1fb234f.1c7c1f3a4843498f88d0f559ff690eb2";
+        //DEBUG
+        System.out.println("Requesting from: " + request_url);
+        //request a json response
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, request_url, (String)null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //get the feed array
+                            JSONArray array = response.getJSONArray("data");
+                            for (int i = 0; i < array.length(); i++) {
+                                //get one feed
+                                JSONObject oneFeed = array.getJSONObject(i);
+                                //get the location block
+                                if (!oneFeed.isNull("location")) {
+                                    JSONObject locationJSON = oneFeed.getJSONObject("location");
+                                    //get the location string
+                                    if (locationJSON != null) {
+                                        String location = locationJSON.getString("name");
+                                        //DEBUG
+                                        System.out.println("FEED: location = " + location);
+                                    }
+                                }
+
+                                //get the comment block
+                                JSONObject commentsJSON = oneFeed.getJSONObject("comments");
+                                //get comment counts
+                                int commentsCount = commentsJSON.getInt("count");
+                                //get comment content
+                                if (commentsCount > 0){
+                                    ArrayList<String> comments = new ArrayList<>();
+                                    //get comment data
+                                    JSONArray commentArray = commentsJSON.getJSONArray("data");
+                                    for (int j = 0; j < commentArray.length(); j++){
+                                        JSONObject oneComment = commentArray.getJSONObject(j);
+                                        //get comment text
+                                        String commentText = oneComment.getString("text");
+                                        JSONObject commentFrom = oneComment.getJSONObject("from");
+                                        String commentName = commentFrom.getString("full_name");
+                                        String comment = commentName + ": " + commentText;
+                                        //DEBUG
+                                        System.out.println("FEED: comment = " + comment);
+                                        comments.add(comment);
+                                    }
+                                }
+                                //get the likes block
+                                JSONObject likesJSON = oneFeed.getJSONObject("likes");
+                                //get likes count
+                                int likesCount = likesJSON.getInt("count");
+                                //get likes content
+                                if (likesCount > 0) {
+                                    ArrayList<String > likes = new ArrayList<>();
+                                    //get likes data
+                                    JSONArray likeArray = likesJSON.getJSONArray("data");
+                                    for (int k = 0; k < likeArray.length(); k++) {
+                                        JSONObject oneLike = likeArray.getJSONObject(k);
+                                        //get like name
+                                        String likeName = oneLike.getString("full_name");
+                                        likes.add(likeName);
+                                        //DEBUG
+                                        System.out.println("FEED: like = " + likeName);
+                                    }
+                                }
+                                //get the image block
+                                JSONObject imageJSON = oneFeed.getJSONObject("images");
+                                //get the standard resoultion block
+                                JSONObject standardResolution = imageJSON.getJSONObject("standard_resolution");
+                                //get the image url
+                                String imageURL = standardResolution.getString("url");
+                                //DEBUG
+                                System.out.println("FEED: image = " + imageURL);
+                                //fetch the image
+                                getFeedImage(imageURL);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        error.printStackTrace();
+                    }
+                });
+        mRequestQueue.add(jsonRequest);
+    }
+
+    private void getFeedImage(String imageURL) {
+        ImageRequest imgRequest = new ImageRequest(imageURL, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                //do something with the bitmap
+            }
+        },0,0, ImageView.ScaleType.FIT_XY, Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+
+                });
+        mRequestQueue.add(imgRequest);
     }
 
 }
--- ParseStarterProject/src/main/java/com/parse/unimelb/Network.java
+++
@@ -1,7 +0,0 @@
-package com.parse.unimelb;
-
-/**
- * Created by songxue on 2/10/2015.
- */
-public class Network {
-}
